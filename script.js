4bfb74952ca531ef4269a811fd48a715dc82dc5d
Sainath <71361447+sainath161@users.noreply.github.com>
2024-03-21 21:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function microtask(name) {
+	console.log(`Processing microtask: ${name}`);
+}
+
+function macrotask(name) {
+	console.log(`Processing macrotask: ${name}`);
+}
+
+setTimeout(() => macrotask('First Macrotask'), 0);
+
+Promise.resolve().then(() => microtask('First Microtask'));
+Promise.resolve().then(() => microtask('Second Microtask'));
+
+setTimeout(() => macrotask('First Macrotask'), 0);
+
+microtask('Third Microtask');
+
+console.log('End of the main script');
